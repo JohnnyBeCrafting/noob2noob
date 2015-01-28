5e0fc66665370dd8e38d075e6e2f3195aa77a68e
jginbound <garces.johnny@gmail.com>
2015-01-28 14:46:47 -0500

    deploying new noob2noob site
    deploying to githubpages

---
+++ _site/assets/scripts/main.js
@@ -0,0 +1,13 @@
+
+// $(document).ready(function(){
+// 	$('.content').hide();
+// 	$('header').hide();
+// 	$('body').html('Welcome to Noob2Noob!');
+// 	var button = $("<button></button");
+// 	button.text('click here to enter');
+// 	$('body').append(button);
+// 	$(button).click(function(){
+// 		alert('hi');
+// 	});
+// });
+// 	
